0681edf0e8bc627e1d3d7486d930ec1879ffa6af
Rubens O Leão <rubensoleao@gmail.com>
2018-05-10 23:37:07 -0300

    3

--- web.py
+++ web.py
@@ -55,7 +55,7 @@ def getFONTS(arq):
 	print(arq)
 	return send_from_directory(ROOT+"/site/static/fonts",arq)
 
-f __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+   app.debug = True
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port)
