7595f5d040a29a9784f3e7bcdbed7201b7ee8621
Rubens O Leão <rubensoleao@gmail.com>
2018-05-10 22:59:58 -0300

    changed to python version 3.5.5

--- flask_app.py
+++ flask_app.py
@@ -55,5 +55,7 @@ def getFONTS(arq):
 	print(arq)
 	return send_from_directory(ROOT+"/site/static/fonts",arq)
 
-if __name__ == "__main__":
-	app.run(host='0.0.0.0', debug=True)
+f __name__ == '__main__':
+     app.debug = True
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host='0.0.0.0', port=port)
