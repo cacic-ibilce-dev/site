fa4df516fa29b06ab886c9da4c32d02e6c92bdef
Rubens O Leão <rubensoleao@gmail.com>
2018-09-14 00:09:28 -0300

    Adicionado a leitura de membros
    Membros são lidos em arquivos .tsv

---
+++ cacic_code.py
@@ -0,0 +1,96 @@
+from glob import glob
+
+#Definindo as constantes
+
+#Diretorios:
+MEMBROS = 'conteudo/membros/*.tsv'
+
+
+
+# retorna listas no senguinte formato:
+# [ [ Ano1, [[nome, apelido,função, mensagem, foto ], [nome, ...]] 
+#   [ Ano2, [[nome, apelido,função, mensagem, foto ], [nome, ...]]
+#	...
+# ]
+#
+# Cada ano é separado em um arquivo tsv(tabela separada por tabs)
+# em que as  colunas são:
+#	0) timestamp
+#	1) email
+#	2) nome
+# 	3) apelido
+#	4) foto
+#	5) função
+#	6) mensagem
+# Esse arquivo .tsv é feito a partir das resposta do formulário do drive
+# que foi passada para as pessoas do CA
+#
+# Como os anos já são ordenados a lista final saíra ordenada
+def Chapas():
+	#Le os arquivos .tsv dentro da pasta membros e retorna uma lista do diretorio
+	# desses arquivos
+	arquivos = glob(MEMBROS)
+	
+	# Lista que conterá todas as chapas
+	Chapas = []
+
+	# Cada Arquivo é uma chapa
+	for diretorio in arquivos:
+		#Abre o arquivo
+		arq = open(diretorio,'r')
+		
+		# Temos que achar o ano da chapa
+		# O ano é o nome do arquivo
+		#Então parseamos o diretorio
+		# 'conteudo/membros/2018.tsv' =split=> [conteudo,membros,2018.tsv]
+		ano = diretorio.split('/')
+		# e pegamos o ultimo elemento da lista
+		# [conteudo,membros,2018.tsv] => [2018.tsv]
+		ano = ano[-1]
+		
+		# '2018.tsv' = tira os ultimos 4 caracteres -> '2018'
+		ano = ano[:-4]
+		print(ano)
+		# Armazenaremaos a chapa em uma lista
+		novaChapa = []
+		# inicializamos pelo ano da chapa
+		novaChapa.append(ano)
+
+		# Criamos uma lista de membros novo, em que cada membro
+		# é uma lista com o [nome, apelido,função, mensagem, foto ]
+		novosMembros = []
+		novoMembro=[]
+
+		# vamos ler os novo membros do arquivo
+		# Cada Arquivo é uma chapa
+		# Cada linha do arquivo é um membro
+		for linha in arq:
+			novoMembro=[] #reseta o membro
+
+			# Os campos do membro são separados por tabulações '\t' que nem especificado
+			# no começo da função
+			# timestamp\temail...\tmensagemsplit('\t') -> [timestamp,email,...,mensagem]
+			linha = linha.split('\t')
+			novoMembro.append(linha[2]) #Nome
+			novoMembro.append(linha[3]) #Apelido
+			novoMembro.append(linha[5]) #Função
+			novoMembro.append(linha[6]) #Mensagem
+			novoMembro.append(linha[4]) #Foto
+
+			novosMembros.append(novoMembro)
+
+
+
+		#deleta a primeira fileira, pois é apenas o cabeçalho do arquivo
+		# (tem o nome das perguntas do google forms)
+		novosMembros = novosMembros[1:]
+		
+		#Adiciona os novosMembros a nova chapa
+		novaChapa.append(novosMembros)
+
+		# adiciona a nova chapa na nossa lista final
+		Chapas.append(novaChapa)
+
+
+
+	return Chapas
--- web.py
+++ web.py
@@ -2,19 +2,34 @@ from flask import Flask, render_template, send_from_directory,send_file
 from flask_flatpages import FlatPages, pygments_style_defs
 import os
 
+import cacic_code as cacic
 
 # Mais informações sobre FLASK:
 # http://flask.pocoo.org/docs/0.12/quickstart/
+
 
 ROOT = os.path.dirname(os.getcwd())
 app = Flask(__name__)
 flatpages = FlatPages(app)
 app.config.from_pyfile('config_flask.cfg')
-INDICES = '/templates/*.html' # Usado para a definição dos linkis
 
 
 #
-#	INDICES
+#	Ler o "banco de dados"
+#
+
+CHAPAS = cacic.Chapas()
+SOBRE = flatpages.get_or_404("cacic_sobre")
+
+
+print("===================")
+for c in CHAPAS:
+	print("ano"+ c[0])
+	for a in c[1]:
+		print(a)
+
+#
+#	INDICES DO SITE
 #
 
 @app.route("/")
@@ -26,7 +41,7 @@ def home():
 @app.route("/sobre.html")
 def sobre():
 	# Pega o arquivo que contém a descrição do CACIC
-	cacic_sobre =  flatpages.get_or_404("cacic_sobre")
+	
 
 	return render_template('sobre.html',cacic_sobre=cacic_sobre)
 
